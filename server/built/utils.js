360bc67cdf04bfe0d26e09566cfe3dd065aab303
petersolimine <peter@beulr.com>
2023-06-30 18:05:46 -0700

    Remove built folder from repository

--- server/built/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatActionsToString = void 0;
-function formatActionsToString(actionsArray) {
-    return actionsArray.map((item) => `${item.user}: ${item.action}`).join("\n");
-}
-exports.formatActionsToString = formatActionsToString;
