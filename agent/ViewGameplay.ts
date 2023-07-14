f0e6fe4089eb0b250ab158ec806d6418f0238957
petersolimine <peter@beulr.com>
2023-07-14 16:43:14 -0700

    a way to view the game when running only an agent

---
+++ agent/ViewGameplay.ts
@@ -0,0 +1,24 @@
+import WebSocket from "ws";
+import { BroadcastMessage } from "../server/lib/WebsocketManager";
+import config from "./config.json";
+
+
+const ws = new WebSocket(`ws://${config.server_url}`);
+
+ws.on("open", () => {
+  console.log("Connected to the WebSocket server.");
+});
+
+ws.on("message", (message: string) => {
+  // convert string to json object of type BroadcastMessage
+  const broadcastMessage: BroadcastMessage = JSON.parse(message);
+  console.log(`${broadcastMessage.name}: ${broadcastMessage.message}`);
+});
+
+ws.on("error", (error: Error) => {
+  console.error(`WebSocket error: ${error.message}`);
+});
+
+ws.on("close", () => {
+  console.log("Disconnected from the WebSocket server.");
+});
--- server/lib/WebsocketManager.ts
+++ server/lib/WebsocketManager.ts
@@ -2,14 +2,14 @@ import WebSocket, { Server as WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 export const clients = new Set<WebSocket>();
 
-type broadcastMessage = {
+export type BroadcastMessage = {
   message: string;
   name: string;
   color: string;
   is_server: boolean;
 };
 
-export const broadcast = (info: broadcastMessage) => {
+export const broadcast = (info: BroadcastMessage) => {
   clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       try {
